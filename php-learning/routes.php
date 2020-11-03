0ad1cbfa9d30d22d8956a50b53044c9126d751ed
Justin Nguyen <65918030+nuggetnchill@users.noreply.github.com>
2020-11-03 09:46:53 -0600

    add a basic PHP & MySQL project for references

---
+++ php-learning/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    'mysandbox/php-learning' => 'controllers/index.php',
+    'mysandbox/php-learning/about' => 'controllers/about.php',
+    'mysandbox/php-learning/about/culture' => 'controllers/about-culture.php',
+    'mysandbox/php-learning/contact' => 'controllers/contact.php'
+]);
